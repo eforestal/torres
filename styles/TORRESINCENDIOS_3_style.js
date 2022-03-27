a0c67efdab28a0d3af601c51a83c04a6c71d643e
eforestal <83982437+eforestal@users.noreply.github.com>
2022-03-27 13:29:21 +0200

    Add files via upload

---
+++ styles/TORRESINCENDIOS_3_style.js
@@ -0,0 +1,39 @@
+var size = 0;
+var placement = 'point';
+
+var style_TORRESINCENDIOS_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "7.800000000000001px \'Ubuntu\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("TORRE") !== null) {
+        labelText = String(feature.get("TORRE"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.04482758620689655,
+                  anchor: [13, 13],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/poi_tower_water.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
